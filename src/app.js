5c7a679fa216787aa777225570e10e9eae2ae937
CaolanCode <cmacmahon3@gmail.com>
2023-02-18 13:18:54 +0000

    commit styling to add button

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import header from './header'
 import './styles/style.css'
 import menu from './menu'
+import link from './google-link'
 
 //create elements
 const container = document.createElement('div')
@@ -9,4 +10,5 @@ container.classList.add('container')
 // append elements
 container.appendChild(header())
 container.appendChild(menu())
+document.head.appendChild(link())
 document.body.appendChild(container)
--- src/menu.js
+++ src/menu.js
@@ -1,9 +1,13 @@
 const menu = () => {
   const panel = document.createElement('div')
   panel.classList.add('menu-container')
+  const title = document.createElement('div')
+  title.classList.add('project-title')
+  title.innerText = 'Projects'
   const addButton = document.createElement('button')
   addButton.classList.add('add-button')
-  addButton.innerText = '+'
+  addButton.innerHTML = '<span class="material-symbols-outlined">add</span><p>Add Project</p>'
+  panel.appendChild(title)
   panel.appendChild(addButton)
   return panel
 }
