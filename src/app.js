e92e748b17708e627739cbfbb0a97c2c2ea776cd
CaolanCode <cmacmahon3@gmail.com>
2023-02-18 14:32:20 +0000

    commit title iput

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import header from './header'
 import './styles/style.css'
 import menu from './menu'
 import link from './google-link'
+import title from './title'
 
 //create elements
 const container = document.createElement('div')
@@ -15,34 +16,12 @@ document.body.appendChild(container)
 
 // elements
 const addButton = document.querySelector('.add-button')
-const menuContainer = document.querySelector('.menu-container')
 
 function getTitle() {
-  const titleContainer = document.createElement('div')
-  titleContainer.classList.add('title-container')
-  // input
-  const input = document.createElement('input')
-  input.type = 'text'
-  input.name = 'title'
-  input.id = 'title'
-  input.placeholder = 'Enter title'
-  // buttons
-  const buttonContainer = document.createElement('div')
-  buttonContainer.classList.add('button-container')
-  const sumbitButton = document.createElement('button')
-  sumbitButton.classList.add('submit-button')
-  sumbitButton.innerText = 'Submit'
-  const cancelButton = document.createElement('button')
-  cancelButton.classList.add('cancel-button')
-  cancelButton.innerText = 'Cancel'
-  buttonContainer.appendChild(sumbitButton)
-  buttonContainer.appendChild(cancelButton)
-  // append
-  titleContainer.appendChild(input)
-  titleContainer.appendChild(buttonContainer)
-  menuContainer.appendChild(titleContainer)
+  const inputTitle = document.getElementById('title')
+  if(inputTitle) return
+  title()
 }
-
 
 // listeners
 addButton.addEventListener('click', getTitle)
---
+++ src/title.js
@@ -0,0 +1,30 @@
+
+const title = () => {
+  const menuContainer = document.querySelector('.menu-container')
+  const titleContainer = document.createElement('div')
+  titleContainer.classList.add('title-container')
+  // input
+  const input = document.createElement('input')
+  input.type = 'text'
+  input.name = 'title'
+  input.id = 'title'
+  input.placeholder = 'Enter title'
+  // buttons
+  const buttonContainer = document.createElement('div')
+  buttonContainer.classList.add('button-container')
+  const sumbitButton = document.createElement('button')
+  sumbitButton.classList.add('submit-button')
+  sumbitButton.innerText = 'Submit'
+  const cancelButton = document.createElement('button')
+  cancelButton.classList.add('cancel-button')
+  cancelButton.innerText = 'Cancel'
+  buttonContainer.appendChild(sumbitButton)
+  buttonContainer.appendChild(cancelButton)
+  // append
+  titleContainer.appendChild(input)
+  titleContainer.appendChild(buttonContainer)
+  menuContainer.appendChild(titleContainer)
+  return menuContainer
+}
+
+export default title
