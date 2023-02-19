b213f77cef9a2dc1b5fd5fb90095ff48d232f080
CaolanCode <cmacmahon3@gmail.com>
2023-02-19 17:15:41 +0000

    commit project button in div

--- src/create-title.js
+++ src/create-title.js
@@ -1,10 +1,15 @@
 const createTitle = () => {
   const titleContainer = document.querySelector('.title-container')
-  const project = document.createElement('button')
-  project.classList.add('project')
+  const projectContainer = document.createElement('div')
+  projectContainer.classList.add('project')
   const titleValue = document.getElementById('title-input').value
-  project.innerHTML = titleValue + '<span class="material-symbols-outlined">close</span>'
-  titleContainer.appendChild(project)
+  projectContainer.innerText = titleValue
+  // exit button
+  const projectButton = document.createElement('button')
+  projectButton.classList.add('delete-project')
+  projectButton.innerHTML = '<span class="material-symbols-outlined">close</span>'
+  projectContainer.appendChild(projectButton)
+  titleContainer.appendChild(projectContainer)
 }
 
 export default createTitle
