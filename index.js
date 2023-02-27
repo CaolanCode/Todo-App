cf3b46e78931e03e174ab738be923b5d65b2434a
CaolanCode <cmacmahon3@gmail.com>
2023-02-27 16:27:39 +0000

    commit task factory function

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const Project = (name) => {
-  const id = createID()
-  const getID = () => id
+  const getID = () => createID()
   const getName = () => name
   const getList = () => []
   return {getID, getName, getList}
+}
+
+const Task = (title, description) => {
+  const getID = () => createID()
+  const getTitle = () => title
+  const getDescription = () => description
+  return {getID, getTitle, getDescription}
 }
 
 function createID() {
